0b87b809f5c5692809ec6c4daaf941f914ff17c7
David Balk Clipel <134098077+DavidClp@users.noreply.github.com>
2026-02-16 21:55:27 -0400

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,1 +1,10 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+#!/bin/bash
+
+while [ ! -f /vagrant/token.txt ]
+do
+  sleep 2
+done
+
+TOKEN=$(cat /vagrant/token.txt)
+
+docker swarm join --token $TOKEN 10.10.10.100:2377
